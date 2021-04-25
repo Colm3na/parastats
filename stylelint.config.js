803f200dd86c4eceb91f03c86b539dea2ca16b0c
mariopino <mariopino@protonmail.com>
2021-04-25 09:42:04 +0200

    lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,6 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    "no-invalid-position-at-import-rule": null
+    'no-invalid-position-at-import-rule': null,
   },
 }
